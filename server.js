996ad96c35e2c824cd17bf84f042d6efb7573c3c
Drunck <211096@astanait.edu.kz>
2022-04-10 22:36:00 +0600

    Added navbar

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname))
+
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/MainPage.html')
-
 })
 app.get('/MainPage', (req, res) => {
     res.sendFile(__dirname + '/MainPage.html')
@@ -15,6 +16,7 @@ app.get('/ProductPage', (req, res) => {
 app.get('/RegistrationPage', (req, res) => {
     res.sendFile(__dirname + '/RegistrationPage.html')
 })
+
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 });

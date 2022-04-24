ed1c1d5c210c49af80c8944434fa791b0d55291f
Drunck <211096@astanait.edu.kz>
2022-04-24 23:26:53 +0600

    Product page with id

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 // routes
 app.use("/registrationPage", require("./routes/RegistrationPage")); // if localhost:3000/RegistrationPage
 app.use("/signInPage", require("./routes/SignInPage")); // if localhost:3000/SignInPage
+app.use("/yourMailAddress", require("./routes/YourMailAddress"));
 app.use("/productSearch", require("./routes/ProductSearch")); // if local
 app.use("/adminPanel", require("./routes/AdminPanel"));
-app.use("/yourMailAddress", require("./routes/YourMailAddress"));
 
 app.get('/', (req, res, next) => {
     res.render("ProductPage", {
@@ -27,6 +27,16 @@ app.get('/', (req, res, next) => {
     })
 })
 
+app.get("/product/:id", (req,res) => {
+    const pId = Number(req.params.id);
+    res.render("Product", {
+        id: pId,
+        ProdNum: 3, // when we'll add DB, we'll can change this
+        price: [5, 3, 7], // the same, like ProdNum
+        currency:'USD',
+        descriptions: ["Some awesome description.", "Very nice description", "Facinating description"],
+    })
+})
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 });
